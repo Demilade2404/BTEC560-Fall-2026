ef4596bbf61db861b528d4bf783fea29f0425898
Demilade2404 <demztof.2004@gmail.com>
2026-09-02 09:56:31 -0400

    Add files via upload

--- BIOL 341.R
+++ BIOL 341.R
@@ -24,3 +24,17 @@ Normalizeddata <- rma(data)
 
 Normalizeddata <- exprs(Normalizeddata)
 boxplot(Normalizeddata)
+
+#Assume first 2 chips are Cancer/Treatment and the next 2 are Normal/Reference chips
+
+dim(Normalizeddata)
+
+Treatment <- Normalizeddata[,c(1,2)]
+Control <- Normalizeddata[,c(3,4)]
+dim(Treatment)
+dim(Control)
+
+#Taking averages on rows for Treatment and Control so we have 1 column for Treatment and 1 column for Control
+
+#Fold change analysis for determining significant genes, Up-regulation and Down-regulation of genes
+#Fold changes can be performed using subtraction and division
