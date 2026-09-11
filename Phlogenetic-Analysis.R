f561175b938e6cfde8512496a1b9d038b9b6fa84
Demilade2404 <demztof.2004@gmail.com>
2026-09-11 09:41:17 -0400

    Add files via upload

---
+++ Phlogenetic-Analysis.R
@@ -0,0 +1,13 @@
+#Author: Ashade, Date: 09/11/2026, Purpose: Phylogenetic analysis
+
+#Load the 4 libraries
+library(ape)
+library(phangorn)
+library(phytools)
+library(geiger)
+
+#Create some dummy string dataset
+
+text.string<- "(((((((cow,pig),whale),(bat,(lemur,human))),(robin,iguana)),coelacanth),gold_fis
+h), shark);" 
+vert.tree<-read.tree(text=text.string)
